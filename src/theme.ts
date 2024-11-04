ff8ba9f6b299dbe4290a150143d07866efa69054
thiago <you@example.com>
2024-11-04 06:45:52 -0300

    feat: config material ui theme, css baseline

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#232323',
+        },
+        secondary: {
+            main: '#4f8e3e',
+        },
+    },
+    typography: {
+        fontFamily:"Helvetica Neve"
+    }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
